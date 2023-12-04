2f839a8d9f6ad39bba52dea49171a89ddf4b04c2
MAHMOUD ATEF <mahmoud.atef3167@gmail.com>
2023-12-04 10:52:13 -0800

    MM

--- lib/core/utils/navigation_manager.dart
+++ lib/core/utils/navigation_manager.dart
@@ -5,30 +5,6 @@ extension NavigationMethods on BuildContext {
     Navigator.pop(this);
   }
 
-  void pushPage(Widget screen) {
-    Navigator.push(
-        this,
-        PageRouteBuilder(
-          pageBuilder: (context, animation, secondaryAnimation) => screen,
-          transitionsBuilder: (context, animation, secondaryAnimation, child) {
-            var begin = const Offset(0.0, 1.0);
-            var end = Offset.zero;
-            var curve = Curves.ease;
-            var tween =
-                Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
-            return SlideTransition(
-              position: animation.drive(tween),
-              child: child,
-            );
-
-            // var begin = const Offset(-1.0, 0);
-            // var end = const Offset(0, 0);
-            // var tween = Tween(begin: begin,end: end);
-            // var offsetAnimation = animation.drive(tween);
-            // return SlideTransition(position: offsetAnimation,child: child,);
-          },
-        ));
-  }
 
   void push(Widget screen) {
     Navigator.push(this, MaterialPageRoute(builder: (context) => screen));
